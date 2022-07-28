69e39e1d40a8f99de69a91ca44aca23a3d40d5cb
brsan <brsandvik@gmail.com>
2022-07-28 03:23:17 -0700

    All tidied up, I even tested it a bit

--- reinforcement_environment.jl
+++ reinforcement_environment.jl
@@ -9,6 +9,18 @@ begin
     using Reinforce
 end
 
+begin  # Parameters and rewards
+    MAX_STEPS = 50
+
+    TARGET_ROOM = Int8(50)
+    CORRECT_DIRECTION = Int8(1)
+    WRONG_DIRECTION = Int8(-1)
+    STUPID_ROOM = Int8(-20)
+    CLOSED_EXIT = Int8(-10)
+    LOOPING_BEHAVIOR = Int8(-100)
+    INVALID_EXIT = Int8(-15)
+end
+
 begin
     const INPUT_VEC_FORMAT = split(INPUT_VEC_ORDER, ',')
     const STATE_WIDTHS = Dict(
@@ -31,11 +43,48 @@ begin
     const STATE_WIDTH = sum(STATE_WIDTHS[x] for x in INPUT_VEC_FORMAT)
 end
 
+struct StrongholdReplay{N, N1}  # boo, and ComputedFieldTypes didn't work (N1 is just N + 1)
+    states::MMatrix{STATE_WIDTH, N1, UInt8}
+    actions::MVector{N, Int8}
+    rewards::MVector{N, Int8}
+    function StrongholdReplay(n)
+        replay = new{n, n+1}(MMatrix{STATE_WIDTH, n+1, UInt8}(undef), MVector{n, Int8}(undef), MVector{n, Int8}(undef))
+        fill!(replay.states, Int8(0))
+        fill!(replay.actions, Int8(-1))
+        replay
+    end
+    StrongholdReplay() = StrongholdReplay(MAX_STEPS)
+end
+
+function Base.iterate(replay::StrongholdReplay{N}, i=1) where N
+    (i > N || replay.actions[i] == -1) && return nothing
+    return (replay.states[:, i], replay.actions[i], replay.rewards[i], replay.states[:, i+1]), i + 1
+end
+
+function Base.length(replay::StrongholdReplay{N}) where N 
+    replay.actions[N] != Int8(-1) && (return N)
+    return searchsortedlast(replay.actions, Int8(-1), lt=(>=))  # a little magic for ya (I didn't want to write binary search myself)
+end
+
+function valid_data(replay)
+    n = length(replay)
+    return states(replay, n), actions(replay, n), rewards(replay, n)
+end
+
+states(replay, n) = view(replay.states, :, SOneTo(n+1))  # For some reason, if you use indexing instead of a view, it takes QUADRATIC COMPILE TIME??? No thank you.
+actions(replay, n) = view(replay.actions, SOneTo(n))
+rewards(replay, n) = view(replay.rewards, SOneTo(n))
+
+states(replay) = states(replay, length(replay))
+actions(replay) = actions(replay, length(replay))
+rewards(replay) = rewards(replay, length(replay))
+
+
 mutable struct StrongholdEnvironment <: AbstractEnvironment
     resettable::Bool
     reward::Int8
     steps::Int
-    state::SVector{STATE_WIDTH, UInt8}
+    replay::StrongholdReplay
     visited_rooms::BitVector
     stronghold::Vector{Room}
     room::Int16
@@ -46,8 +95,8 @@ mutable struct StrongholdEnvironment <: AbstractEnvironment
     function StrongholdEnvironment(s::Vector{Room}, r::Bool=true, target_piece::UInt8=UInt8(11))
         # This assumes you start in the portal room if you want to go anywhere else (pre-1.9 mode)
         room::Int16 = target_piece == 11 ? length(s) : findfirst(x -> x.piece == 11, s)
-        env = new(r, 0, 0, (@SVector zeros(UInt8, STATE_WIDTH)), falses(length(s)), s, room, 14, 0, 0, target_piece)
-        env.state = get_state(env)
+        env = new(r, 0, 0, StrongholdReplay(), falses(length(s)), s, room, 14, 0, 0, target_piece)
+        set_state!(env)
         env
     end
 end
@@ -94,31 +143,24 @@ end
 
 begin
     current(env::StrongholdEnvironment)::Room = env.stronghold[env.room]
-    function get_state(env::StrongholdEnvironment)  # This runs in <100ns :)
+    function get_state(env)  # This runs in <100ns :)
         c = current(env)
         output = @MVector zeros(UInt8, STATE_WIDTH)
         output[SCALAR_FUNCTION_OFFSETS] .= SVector{NUM_SCALAR_FUNCTIONS}(x(env, c) for x in SCALAR_FUNCTION_LIST)
         output[VECTOR_FUNCTION_OFFSETS .+ SVector{NUM_VECTOR_FUNCTIONS}(x(env, c) for x in VECTOR_FUNCTION_LIST)] .= 1
         return output
     end
-end
-
-begin  # Parameters and rewards
-    MAX_STEPS = 50
-
-    TARGET_ROOM = Int8(50)
-    CORRECT_DIRECTION = Int8(1)
-    WRONG_DIRECTION = Int8(-1)
-    STUPID_ROOM = Int8(-20)
-    CLOSED_EXIT = Int8(-10)
-    LOOPING_BEHAVIOR = Int8(-100)
-    INVALID_EXIT = Int8(-15)
+    # Despite literally rearranging the entire codebase so I could do in-place mutates... they weren't faster. compiler couldn't figure it out.
+    function set_state!(env)  
+        env.replay.states[:, env.steps+1] = get_state(env)
+        return  # saves like 20 ns to not return :P
+    end
 end
 
 begin
     Reinforce.finished(env::StrongholdEnvironment, s′) = current(env).piece == env.target_piece
-    Reinforce.actions(env::StrongholdEnvironment, s) = UInt8(0):UInt8(5)
-    Reinforce.state(env::StrongholdEnvironment) = env.state
+    Reinforce.actions(env::StrongholdEnvironment, s) = Int8(0):Int8(5)
+    Reinforce.state(env::StrongholdEnvironment) = view(env.replay.states, :, env.steps+1)
     function Reinforce.reset!(env::StrongholdEnvironment)
         env.resettable || return env
         env.stronghold = strongholds[rand(train)]
@@ -129,7 +171,7 @@ begin
         env.entry = 0
         env.reward = 0
         env.steps = 0
-        env.state = get_state(env)
+        env.replay = StrongholdReplay()
         return env
     end
     function Reinforce.step!(env::StrongholdEnvironment, s, a)
@@ -137,8 +179,10 @@ begin
         c = current(env)
         if a > PIECE_TO_NUM_EXITS[c.piece] || a == 0 && c.piece == 13  # Chose an exit that can never have a room
             env.reward = INVALID_EXIT
+            env.replay.states[:, env.steps+1] = env.replay.states[:, env.steps]
         elseif a != 0 && c.exits[a] <= 0  # Chose an exit that doesn't have a room
             env.reward = CLOSED_EXIT
+            env.replay.states[:, env.steps+1] = env.replay.states[:, env.steps]
         else
             # If you have already been in this room, and you go back the way you came, you're looping.
             if env.visited_rooms[env.room] && a == env.entry 
@@ -156,7 +200,7 @@ begin
                 env.room = c.exits[a]
                 env.entry = 0
             end
-            env.state = get_state(env)
+            set_state!(env)
         end
         c = current(env)
         if c.piece == env.target_piece  # Success! Much reward.
@@ -164,33 +208,12 @@ begin
         elseif c.piece > 9  # It happens that this is all of the stupid rooms (rooms that never have an exit other than the entrance)
             env.reward = min(env.reward, STUPID_ROOM)
         end
+        env.replay.rewards[env.steps] = reward(env)
+        env.replay.actions[env.steps] = a
         return reward(env), state(env)
     end
     Reinforce.ismdp(::StrongholdEnvironment) = false
     Reinforce.maxsteps(::StrongholdEnvironment) = MAX_STEPS
-end
-
-begin
-    struct FirstExitPolicy <: AbstractPolicy end
-    Reinforce.action(π::FirstExitPolicy, r, s, A) = 1    
-end
-
-struct StrongholdReplayBuffer
-    states::SizedVector{MAX_STEPS + 1, SVector{STATE_WIDTH, UInt8}}  # Float32 to avoid converting to Float32 multiple times later
-    actions::SVector{MAX_STEPS, UInt8}
-    rewards::SVector{MAX_STEPS, Int8}
-end
-
-function Base.iterate(rb::StrongholdReplayBuffer, i=1)
-    (i > MAX_STEPS || rb.actions[i] == -1) && return nothing
-    return (rb.states[i], rb.actions[i], rb.rewards[i], rb.states[i+1]), i + 1
-end
-
-begin
-    # Oracle policies need to have the environment to know which way to go, so need to be treated differently by functions.
-    abstract type AbstractOraclePolicy <: AbstractPolicy end
-    mutable struct OraclePolicy <: AbstractOraclePolicy env::Union{StrongholdEnvironment, Nothing} end
-    Reinforce.action(π::OraclePolicy, r, s, A) = current(π.env).targetDirection
 end
 
 return
--- utils.jl
+++ utils.jl
@@ -17,5 +17,24 @@ function print_correct_path(stronghold)
         println(a, "\t", r)
         print_room(env)
     end
+    return env.replay
 end
 
+function random_replay()
+    env = StrongholdEnvironment(rand(strongholds), false)
+    p = RandomPolicy()
+    for _ in Episode(env, p); end
+    return env.replay
+end
+
+begin
+    struct FirstExitPolicy <: AbstractPolicy end
+    Reinforce.action(π::FirstExitPolicy, r, s, A) = 1    
+end
+
+begin
+    # Oracle policies need to have the environment to know which way to go, so need to be treated differently by functions.
+    abstract type AbstractOraclePolicy <: AbstractPolicy end
+    mutable struct OraclePolicy <: AbstractOraclePolicy env::Union{StrongholdEnvironment, Nothing} end
+    Reinforce.action(π::OraclePolicy, r, s, A) = current(π.env).targetDirection
+end
